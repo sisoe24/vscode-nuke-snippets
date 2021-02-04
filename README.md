813645fea9fc66bc51cfbd4cde6c937b9f7814ed
virgilsisoe <sisoe24@gmail.com>
2021-02-04 17:24:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# nuke-snippets README
+
+Visual Studio Code snippets for Nuke Python API.
+
+## Features
+
+The [Nuke Python API documentation](https://learn.foundry.com/nuke/developers/70/pythonreference) converted to snippets. Snippets include
+description and arguments with vscode snippet style placeholders:
+
+![example](images/examplex.gif)
+
+## Known Issues
+
+* The documentation does not always list the arguments or makes it clear to which is optional. Some of the snippets include a long description where you can understand better,
+but some of them won't (again it depends on the web documentation).
+* Because the snippets are automatically generated, I could not verify manually that everything is absolutely correct.
+
+## Release Notes
+
+* The nuke modules availiable localy are missing from the snippets, as you could obtain a better autocomplete if you reference them.
+* Classes magic methods are not included.
+* Class methods snippets are included but they should be better used as a _peak view_ in the API. An example would be `nuke.Node().channels()`; yes you can add an argument to `Node()` and create one, but is likely that you already have a variabile with that type of object: `n = nuke.createNode('Tracker')`. In that case you could "sneak peak" the API to see what methods are availabile for a `Node` object
+
+Because snippets are automatically generated from the online documentation,
+is likely that is going to be bloated with a lot of text, so I would suggest to
+enable the extension only in your Nuke scripting directory/workspace.
+
+### 0.0.1
+
+Initial release of nuke-python-snippets
---
+++ src/nuke_snippets.py
@@ -0,0 +1,287 @@
+"""Generate nuke snippets from online documentation."""
+import os
+import json
+import logging
+import textwrap
+
+from typing import Generator
+from typing import Union
+
+import requests
+import regex
+from bs4 import BeautifulSoup
+
+
+# TODO: convert double qoutes to single quotes?
+
+logging.basicConfig(format='%(levelname)s: %(message)15s',
+                    filename='log/report.log', filemode='w',
+                    level=logging.INFO)
+
+SNIPPETS = {}
+
+
+def clean_header(function: str) -> str:
+    """Clean function header text.
+
+    Some functions arugments are splitted into multiple lines, so they need
+    to be put back together and clean the empty spaces left.
+
+    Args:
+        function (str): function text to clean.
+
+    Returns:
+        str: function header text 1 line.
+    """
+    logging.debug('Header to clean: %s', function)
+
+    func_header = ' '.join(textwrap.wrap(function))
+
+    patterns = {
+        r'\s{2,}': ' ',
+        r'self,?': ''
+    }
+
+    for pattern, sub in patterns.items():
+        func_header = regex.sub(pattern, sub, func_header)
+
+    logging.info('Header: %s', func_header)
+
+    return func_header
+
+
+def clean_description(function: str) -> str:
+    """Clean function description text.
+
+    Args:
+        function (str): function text to clean.
+
+    Returns:
+        str: function description text 1 line.
+    """
+    logging.debug("Description to clean: %s", function)
+
+    patterns = {
+        r'source\scode': '',
+        r'(\n){2,}': '\n\n'
+    }
+
+    for pattern, sub in patterns.items():
+        function = regex.sub(pattern, sub, function.strip())
+
+    logging.info('Description: %s', function)
+    return function
+
+
+def insert_placeholders(args: str, body: str) -> str:
+    """Insert placeholder sign "$" to arguments inside the snippets body.
+
+    Args:
+        body (str): body snippet
+
+    Returns:
+        str: body snippet with placeholders
+    """
+    args_list = [arg.strip() for arg in args.split(',')]
+    logging.debug('Args list: %s', args_list)
+    placeholders_list = []
+
+    for index, arg in enumerate(args_list, 1):
+        placeholder = "${%i:%s}" % (index, arg)
+
+        # if argument is a keyword
+        if keywords := regex.search(r'(\w+=)(.+)', arg):
+            keyword, arg = keywords.groups()
+            placeholder = "%s${%i:%s}" % (keyword, index, arg)
+
+        placeholders_list.append(placeholder)
+
+    body = body.replace(f'({args})', f'({", ".join(placeholders_list)})')
+
+    logging.debug('Args placeholder: %s', placeholders_list)
+    logging.info('New Header: %s', body)
+    return body
+
+
+def extract_args(function: str) -> str:
+    """Extract function arguments.
+
+    Args:
+        function (str): function text to extract arguments.
+
+    Returns:
+        str: new function text with placeholder arguments.
+    """
+    if args := regex.search(r'(?<=\()(.+)(?=\))', function):
+        function = insert_placeholders(args.group(), function)
+    return function
+
+
+def snippets_template(title: str, prefix: str, body: str, description: str) -> dict:
+    """Vscode snippets template.
+
+    Args:
+        module (str): nuke module of the method
+        title (str): title of the snippet
+        prefix (str): prefix of the snippet
+        body (str): body of the snippet
+        description (str): description of the snippet
+
+    Returns:
+        dict: [description]
+    """
+    return {
+        title: {
+            "prefix": prefix,
+            "body": body,
+            "description": description
+        }
+    }
+
+
+def function_details(page: BeautifulSoup) -> Generator:
+    """Get the functions information in from the page details."""
+    for child in page.find_all('table', attrs={'class': 'details'}):
+        yield child.text.strip()
+
+
+def functions(page: BeautifulSoup, section: str) -> Generator:
+    """Get the functions information in from the table.
+
+    @param section: this is a function.
+    """
+    table = page.find('a', attrs={'name': f'section-{section}'})
+    function_table = table.next_element.next_element
+    func_summary = function_table.findChildren(
+        'td', attrs={'class': 'summary'})
+
+    summary_returns = ""
+    index = 0
+    for summary in func_summary:
+        summary = summary.text
+
+        if (index % 2) == 0:
+            summary_returns = summary
+        else:
+            if regex.search(r'\w', summary_returns):
+                summary_returns = regex.sub(
+                    r'\n', ' ', summary_returns, regex.ENHANCEMATCH)
+                summary = f"{summary}Returns ->\b{summary_returns}"
+
+        index += 1
+        yield summary.strip()
+
+
+def class_link_page(page: BeautifulSoup) -> str:
+    """Extract link reference for nuke classes.
+
+    Args:
+        page (BeautifulSoup): BeautifulSoup object to parse.
+
+    Yields:
+        [str]: nuke class link reference
+    """
+    section = page.find('a', attrs={'name': 'section-Classes'})
+    class_section = section.next_element.next_element
+
+    for i in class_section.findChildren('a'):
+        link = i.get('href')
+        if not link.startswith('#'):
+            yield str(link)
+
+
+def generate_snippets(page, module, section):
+    for generator in (functions(page, section), function_details(page)):
+        for text in generator:
+            logging.info('\n')
+            logging.debug(text)
+
+            if func := regex.match(r'^(.+?\))\n(.+)', text, regex.S):
+                logging.debug('Regex match for: %s = %s', text, func)
+
+                func_header = clean_header(func.group(1))
+
+                # XXX: ignore dunder methods for now?
+                if func_header.startswith('__'):
+                    continue
+
+                func_description = clean_description(func.group(2))
+                func_new_header = extract_args(func_header)
+
+                SNIPPETS.update(snippets_template(
+                    title=f'{module}.{func_header}',
+                    prefix=f'{module}.{func_header}',
+                    body=f'{module}.{func_new_header}',
+                    description=func_description
+                ))
+
+            if regex.search('Variables Details', text):
+                break
+
+
+def parse_nuke_module(reference_url: str):
+    """Parse and extract snippets from nuke main module web page
+
+    Args:
+        reference_url (str): web starting page of nuke modules
+
+    Returns:
+        generator: a generator with nuke classes links.
+    """
+    nuke_url = reference_url + '/nuke-module.html'
+    html_text = requests.get(nuke_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    generate_snippets(soup, 'nuke', section='Functions')
+
+    return class_link_page(soup)
+
+
+def parse_class_module(reference_url: str, links: Union[Generator, list]):
+    """Parse and extract snippets from the nuke classes module web page.
+
+    Args:
+        reference_url (str): web link of the page to parse
+        links (str): web link of the page to parse
+    """
+    for link in links:
+
+        class_link = os.path.join(reference_url, link)
+
+        html_text = requests.get(class_link).text
+        soup = BeautifulSoup(html_text, 'html.parser')
+
+        class_module = link.replace('-class.html', '') + '()'
+
+        print(f'==> DEBUG: class_module: {class_module}')
+
+        generate_snippets(soup, class_module, section='InstanceMethods')
+
+
+def generate_json(snippets: dict, filename=''):
+    """Create json snippets file.
+
+    Args:
+        snippets (dict): vscode snippets to write into json
+        filename (str, optional): optional filename. Defaults to ''.
+    """
+    snippets_filepath = f'snippets/{filename}snippets.code-snippets'
+
+    with open(snippets_filepath, 'w') as snippets_file:
+        snippets_file.write("{}")
+        snippets_file.seek(0)
+        json.dump(snippets, snippets_file, indent=4)
+
+
+def main():
+    """Execute main."""
+    reference_url = 'https://learn.foundry.com/nuke/developers/70/pythonreference'
+    class_links = parse_nuke_module(reference_url)
+    parse_class_module(reference_url, class_links)
+    generate_json(SNIPPETS)
+
+
+if __name__ == '__main__':
+    main()
+
+nuke.tprint(value, sep=' ', end='\n', file=sys.stdout, ...)
